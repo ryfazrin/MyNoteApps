d7a752a5af0e8e2bd916ba36145df3ea3dede9a6
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-10-23 20:23:22 +0800

    Revert "Create Entity Note"
    This reverts commit 1a1449dbbf7377a3841c27ead011d1c1e64a61a4.

--- app/src/main/java/com/ryfazrin/mynoteapps/entity/Note.kt
+++
@@ -1,8 +0,0 @@
-package com.ryfazrin.mynoteapps.entity
-
-data class Note(
-    var id: Int = 0,
-    var title: String? = null,
-    var description: String? = null,
-    var date: String? = null
-)
