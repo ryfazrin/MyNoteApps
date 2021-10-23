55dfa60f529c19d465087caad0aa3c374da96bb0
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-10-23 23:37:24 +0800

    Update NoteAdapter.kt
    Setelah itu generate getter untuk arraylist-nya dan juga constructor untuk interface OnItemClickCallBack.
    
    Interface ini dibutuhkan karena kita ingin membaca hasil klik pada item RecyclerView di MainActivity.

--- app/src/main/java/com/ryfazrin/mynoteapps/adapter/NoteAdapter.kt
+++ app/src/main/java/com/ryfazrin/mynoteapps/adapter/NoteAdapter.kt
@@ -3,8 +3,16 @@ package com.ryfazrin.mynoteapps.adapter
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.ryfazrin.mynoteapps.entity.Note
 
 class NoteAdapter : RecyclerView.Adapter<NoteAdapter.NoteViewHolder>() {
+    var listNotes = ArrayList<Note>()
+        set(listNotes) {
+            if (listNotes.size > 0) {
+                this.listNotes.clear()
+            }
+            this.listNotes.addAll(listNotes)
+        }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteAdapter.NoteViewHolder {
         TODO("Not yet implemented")
@@ -20,6 +28,10 @@ class NoteAdapter : RecyclerView.Adapter<NoteAdapter.NoteViewHolder>() {
 
     inner class NoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
+    }
+
+    interface OnItemClickCallback {
+        fun  onItemClicked(selectedNote: Note?, position: Int)
     }
 
 }
