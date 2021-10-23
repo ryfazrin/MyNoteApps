42f6496488137d8efb100b07c5b6f81ae8ca6eb1
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-10-23 22:40:55 +0800

    Create NoteHelper.kt
    Deklarasikan variabel dan constructor nya

---
+++ app/src/main/java/com/ryfazrin/mynoteapps/db/NoteHelper.kt
@@ -0,0 +1,16 @@
+package com.ryfazrin.mynoteapps.db
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import com.ryfazrin.mynoteapps.DatabaseHelper
+import com.ryfazrin.mynoteapps.db.DatabaseContract.NoteColumns.Companion.TABLE_NAME
+
+class NoteHelper(context: Context) {
+
+    private var databaseHelper: DatabaseHelper = DatabaseHelper(context)
+    private lateinit var database: SQLiteDatabase
+
+    companion object {
+        private const val DATABASE_TABLE = TABLE_NAME
+    }
+}
