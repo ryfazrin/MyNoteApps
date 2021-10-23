3cda522e41cd6c509e44ce2716dba07cf9b789e4
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-10-23 23:32:24 +0800

    Update NoteAdapter.kt
    Implementasikan metode yang dibutuhkan oleh Recyclerview.Adapter

--- app/src/main/java/com/ryfazrin/mynoteapps/adapter/NoteAdapter.kt
+++ app/src/main/java/com/ryfazrin/mynoteapps/adapter/NoteAdapter.kt
@@ -1,4 +1,25 @@
 package com.ryfazrin.mynoteapps.adapter
 
-class NoteAdapter {
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class NoteAdapter : RecyclerView.Adapter<NoteAdapter.NoteViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteAdapter.NoteViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: NoteAdapter.NoteViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    inner class NoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
 }
