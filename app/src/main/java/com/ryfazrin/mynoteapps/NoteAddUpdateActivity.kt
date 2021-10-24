229c72e40910f5d6abb965a99354ab04b9091c79
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-10-24 09:21:14 +0800

    Declarete Variable in NoteAddUpdateActivity

--- app/src/main/java/com/ryfazrin/mynoteapps/NoteAddUpdateActivity.kt
+++ app/src/main/java/com/ryfazrin/mynoteapps/NoteAddUpdateActivity.kt
@@ -2,8 +2,29 @@ package com.ryfazrin.mynoteapps
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ryfazrin.mynoteapps.databinding.ActivityNoteAddUpdateBinding
+import com.ryfazrin.mynoteapps.db.NoteHelper
+import com.ryfazrin.mynoteapps.entity.Note
 
 class NoteAddUpdateActivity : AppCompatActivity() {
+
+    private var isEdit = false
+    private var note: Note? = null
+    private var position: Int = 0
+    private lateinit var noteHelper: NoteHelper
+
+    private lateinit var binding: ActivityNoteAddUpdateBinding
+
+    companion object {
+        const val EXTRA_NOTE = "extra_note"
+        const val EXTRA_POSITION = "extra_position"
+        const val RESULT_ADD = 101
+        const val RESULT_UPDATE = 201
+        const val RESULT_DELETE = 301
+        const val ALERT_DIALOG_CLOSE = 10
+        const val ALERT_DIALOG_DELETE = 20
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_note_add_update)
